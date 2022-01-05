2fc075f49ce74bc4e2ebe59eeb964acafa299f2a
James Hubert <jwhubert91@gmail.com>
2022-01-04 21:22:50 -0800

    Create app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [ counter, setCounter ] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.viewContainer}>
+      <Text style={styles.header}>React Native Counter App</Text>
+      <View style={styles.bodyContainer}>
+        <Text style={styles.counterOutput}>{counter}</Text>
+        <View style={styles.buttonsContainer}>
+          <Button title="+" onPress={ ()=> setCounter( counter + 1 ) } />
+          <Button title="-" onPress={ ()=> setCounter( counter - 1 ) } />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  viewContainer: {
     backgroundColor: '#fff',
+    padding: 5,
+    flex: 1,
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  bodyContainer: {
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
+  },
+  counterOutput: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  buttonsContainer: {
+    width: '50%',
+    height: 80,
+    marginTop: 30,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  button: {
+    marginVertical: 5,
   },
 });
